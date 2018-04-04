ccb0112084644c95a80dba80c565e921e0308fe1
D7703_23 <d7703_23@D7703-23ui-iMac.local>
2018-04-04 11:43:20 +0900

    카운터

--- Alien Animation/ViewController.swift
+++ Alien Animation/ViewController.swift
@@ -9,15 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var counter: UILabel!
+    var index = 1
+    var direction = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // 첫번째 이미지 보이기
+        myImageView.image = UIImage(named: "frame1.png")
+        counter.text = "1"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func update(_ sender: Any) {
+        if index == 5{
+            direction = 0
+        }
+        else if index == 1{
+            direction = 1
+        }
+        if direction == 1{
+            index += 1
+        }
+        else if direction == 0{
+            index = index - 1
+        }
+        myImageView.image = UIImage(named: "frame\(index).png")
+        counter.text = String(index)
     }
 
 
