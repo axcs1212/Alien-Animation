89cbc215f4cc2e8925c8a5284b8a13a0e93cdd1b
D7703_23 <d7703_23@D7703-23ui-iMac.local>
2018-04-04 12:44:35 +0900

    카운터 주석추가

--- Alien Animation/ViewController.swift
+++ Alien Animation/ViewController.swift
@@ -23,22 +23,24 @@ class ViewController: UIViewController {
     }
 
     @IBAction func update(_ sender: Any) {
+        //index가 5일때 direction에 값을 0으로 해준다
         if index == 5{
             direction = 0
         }
+            //index가 1이면 direction도 값이 1이된다.
         else if index == 1{
             direction = 1
         }
+        //direction이 1이면 인덱스 값을 증가시킨다.
         if direction == 1{
             index += 1
         }
+            //direction이 0이면 index 값을 감소시킨다.
         else if direction == 0{
             index = index - 1
         }
         myImageView.image = UIImage(named: "frame\(index).png")
         counter.text = String(index)
+
     }
-
-
 }
-
